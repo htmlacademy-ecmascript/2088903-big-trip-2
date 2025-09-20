5e71d79ce272297f89077c4f67233e92a06e9013
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2025-09-20 14:20:46 +0300

    Упрощает функции-компараторы

--- src/utils/sort.js
+++ src/utils/sort.js
@@ -2,11 +2,6 @@ import dayjs from 'dayjs';
 
 export const sortFromMaxPrice = (a, b) => b.base_price - a.base_price;
 
-export const sortFromMaxDuration = (a, b) => {
-  const timeA = dayjs(a.date_to).diff(dayjs(a.date_from));
-  const timeB = dayjs(b.date_to).diff(dayjs(b.date_from));
+export const sortFromMaxDuration = (a, b) => dayjs(b.date_to).diff(dayjs(b.date_from)) - dayjs(a.date_to).diff(dayjs(a.date_from));
 
-  return timeB - timeA;
-};
-
-export const sortByDate = (pointA, pointB) => dayjs(pointA.date_from).diff(dayjs(pointB.date_from));
+export const sortByDate = (a, b) => dayjs(a.date_from).diff(dayjs(b.date_from));
